96da2dbd4a8f06d336ba0c05ab98b674d959cd37
theraginggoblin <43911061+theraginggoblin@users.noreply.github.com>
2024-05-05 18:17:16 +1000

    Feature/cichecks (#1)
    * jest dev package install nodejs and move server files
    
    * forgot add for the file moves
    
    * update ci check yaml file
    
    * refactor web server for easier testing
    
    * better styling
    
    * refactor again for better parsing of args, docstrings and unit tests
    
    * shell script update and remove console.log
    
    * make parsed args return more proper with object instead of array
    
    * imports and unit test update
    
    * unit test and ci workflow updates

--- shell_scripts/build_and_run_localhost_server.sh
+++ shell_scripts/build_and_run_localhost_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sh build_localhost_server.sh
-
-node ../server.js --protocol https --port 5050
+sh build_localhost_web_server.sh
+sh run_web_server_unit_tests.sh
+node ../web_server/src/main.js --protocol https --port 5050
---
+++ web_server/src/main.js
@@ -0,0 +1,14 @@
+/*
+Entry point for the PTFI web server.
+*/
+
+// imports
+const { PTFIWebsite } = require('./server');
+const process = require('process');
+const minimist = require('minimist');
+
+if (require.main == module) {
+  const ARGS = minimist(process.argv.slice(2));
+  site = new PTFIWebsite(ARGS);
+  site.run();
+}
