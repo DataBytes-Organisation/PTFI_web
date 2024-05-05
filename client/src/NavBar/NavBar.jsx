488b20ccfc098e4d16bdb56269de6e57de02a215
ywaliatin <87930065+ywaliatin@users.noreply.github.com>
2024-05-05 17:25:28 +1000

    responsive navbar and homepage

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import logo from '../Images/Logo.png';
 import { HashLink as Link } from 'react-router-hash-link';
 
-
 function NavBar() {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    // Function to toggle navigation visibility
+    const toggleNav = () => {
+        if (window.innerWidth <= 768) {
+            setIsNavVisible(!isNavVisible);
+        }
+    };
+
     return (
         <div id="navbar">
-            {/*logo should be imported added to a  resource folder and referenced appropriately*/}
             <div id="logo-container">
-                {/* Wrap the image with a Link component */}
                 <Link to="/#company-vision">
                     <img id="logo" src={logo} className="logo" alt="Logo"/>
-                </Link>       </div>
-
-            <div id="nav-links">
+                </Link>
+            </div>
+            <div className="hamburger-menu" onClick={toggleNav}>
+                ☰
+            </div>
+            <div id="nav-links" className={isNavVisible ? "display-flex" : ""}>
                 <Link to="/AboutUs" className="nav-link">About us</Link>
                 <Link to="/Membership" className="nav-link">Membership</Link>
                 <Link to="/Publication" className="nav-link">Publication</Link>
                 <Link to="/Technical" className="nav-link">Technical Communities</Link>
+                <Link to="/Join" className="nav-link" id="join-button">Join PTFI</Link>
+                <Link to="/Registration" className="nav-link" id="registration-button">Registration</Link>
             </div>
-            <Link to="/Join" id="join-button">Join PTFI</Link>
-            <Link to="/Registration" id="registration-button">Registration</Link>
         </div>
     );
 }
