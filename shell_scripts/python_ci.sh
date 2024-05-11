0f9dac0ed49413abd2f07fc4e8688a05853bf24d
Adam <amedwards@deakin.edu.au>
2024-05-11 23:14:36 +1000

    updates

--- shell_scripts/python_ci.sh
+++ shell_scripts/python_ci.sh
@@ -6,5 +6,6 @@ source $VIRTUAL_ENV/Scripts/Activate
 pip install -r test_requirements.txt
 coverage run -m pytest
 coverage report -m
-python -m pylint
+python -m pylint ./src --fail-under=8
+python -m pylint ./tests --fail-under=8
 cd ../shell_scripts
