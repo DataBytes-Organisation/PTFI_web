b016a53043e1696fb8d8dcc9ce04ce1e718686be
Adam <amedwards@deakin.edu.au>
2024-05-12 00:06:16 +1000

    fix

--- shell_scripts/build_python_backend.sh
+++ shell_scripts/build_python_backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VIRTUAL_ENV=".venv"
 cd ../python_backend
-python -m venv $VIRTUAL_ENV
+[ ! -d "./$VIRTUAL_ENV" ] && python -m venv $VIRTUAL_ENV
 source $VIRTUAL_ENV/Scripts/Activate
-pip install -r requiremements.txt
+pip install -r requirements.txt
 cd ../shell_scripts
