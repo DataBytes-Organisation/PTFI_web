a7b48bc636b7605c66c510e5944cc79a6f45f83f
mustafa-deakin <128792668+mustafa-deakin@users.noreply.github.com>
2024-12-10 14:07:28 +1100

    Add files via upload
    Signed-off-by: mustafa-deakin <128792668+mustafa-deakin@users.noreply.github.com>

--- client/src/Membership/MembershipPage.jsx
+++ client/src/Membership/MembershipPage.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MembershipPage.css';
+import VisionSection from './VisionSection/VisionSection';
+import SignUpSection from './SignUpSection/SignUpSection';
+import BenefitsSection from './BenefitsSection/BenefitsSection';
+import MembershipInfoSection from './MembershipInfoSection/MembershipInfoSection';
 
 function MembershipPage() {
   return (
-    <div id='MembershipPage'>
-      <p> PLACEHOLDER TEST TEXT MEMBERSHIP</p>  
+    <div id="membership-page">
+      <div className="vision-wrapper">
+        <VisionSection />
+      </div>
+      <div className="membership-content">
+        {/*SignUp Section */}
+        <div className="row">
+          <SignUpSection />
+        </div>
+
+        {/*Benefits Section*/}
+        <div className="row reverse">
+          <BenefitsSection />
+        </div>
+
+        {/*Membership Info Section */}
+        <MembershipInfoSection />
+      </div>
     </div>
   );
 }
